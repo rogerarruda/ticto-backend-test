640fb234539773bf3e728b384674a4fb61f6da64
Roger Arruda <roger@interative.info>
2025-08-28 21:33:34 -0300

    Adicionando LogoutController e rota de logout

--- routes/api.php
+++ routes/api.php
@@ -8,4 +8,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/login', Api\Auth\LoginController::class);
 
-Route::get('/user', fn (Request $request) => $request->user())->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', fn (Request $request) => $request->user());
+    Route::post('/logout', Api\Auth\LogoutController::class);
+});
