4c9e8a50b4e8ad308bfb7012c70409bc36f64be5
Roger Arruda <roger@interative.info>
2025-08-28 21:25:50 -0300

    Adicionando LoginController, LoginRequest e rota de login para autenticação via API

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,10 @@
 
 declare(strict_types = 1);
 
+use App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::post('/login', Api\Auth\LoginController::class);
 
 Route::get('/user', fn (Request $request) => $request->user())->middleware('auth:sanctum');
